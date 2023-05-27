6ab9161fb96c686ef298ef604a486dc04c0e6ed8
sebastian <sebastian.martinez07@uptc.edu.co>
2023-05-27 09:21:40 -0500

    frontend with React

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask,jsonify
 from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def index():
