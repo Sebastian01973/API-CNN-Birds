97f91a3c36425c02342003f7dc02a824d2554e87
sebastian <sebastian.martinez07@uptc.edu.co>
2023-05-28 11:40:37 -0500

    Home basic :D

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,56 @@ import ImageUpload from "./components/ImageUpload";
 import Home from "./Pages/Home";
 import Page404 from "./Pages/Page404";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./Pages/Navbar";
 
 import "./App.css";
 
 class App extends Component {
   render() {
+    
+    const galleryImages = [
+      {
+        img: "/img/1.jpg",
+      },
+      {
+        img: "/img/2.jpg",
+      },
+      {
+        img: "/img/3.jpg",
+      },
+      {
+        img: "/img/4.jpg",
+      },
+      {
+        img: "/img/5.jpg",
+      },
+      {
+        img: "/img/6.jpg",
+      },
+      {
+        img: "/img/7.jpg",
+      },
+      {
+        img: "/img/8.jpg",
+      },
+      {
+        img: "/img/9.jpg",
+      },
+      {
+        img: "/img/10.jpg",
+      },
+    ];
     return (
       <Router>
         <div className="bg-dark text-light">
           <Navbar />
           <div className="container p-4">
             <Routes>
-              <Route path="/" element={<Home />} exact />
+              <Route
+                path="/"
+                element={<Home galleryImages={galleryImages} />}
+                exact
+              />
               <Route path="/upload" element={<ImageUpload />} />
               <Route path="*" element={<Page404 />} />
             </Routes>
--- frontend/src/components/ImageUpload.js
+++ frontend/src/components/ImageUpload.js
@@ -1,44 +1,85 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+
+import PredictionModel from "./PredictionModel";
 
 const ImageUpload = () => {
-
+  const [prediction, setPrediction] = useState("");
   const [image, setImage] = useState(null);
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const [isData, setIsData] = useState(false);
 
   const handleChange = (e) => {
-    console.log(e.target.files[0]);
     setImage(e.target.files[0]);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
     const formData = new FormData();
-    formData.append('image', image);
+    formData.append("image", image);
 
-    //Envia al backend la foto uwu
-    const res = await axios.post('http://127.0.0.1:5000/upload', formData);
+    //Envia al backend la foto
+    const res = await axios.post("http://127.0.0.1:5000/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress(progressEvent) {
+        const { loaded, total } = progressEvent;
+        let percent = parseInt((loaded * 100) / total);
+        setUploadPercentage(percent);
+      },
+    });
 
-    console.log(res);
+    setLoading(false);
+    setUploadPercentage(0);
+
+    setPrediction(res.data);
+
+    setIsData(true);
   };
-  
+
   return (
-    <div className='col-md-4 offset-md-4'>
-      <div className='card bg-dark text-light rounded-0 p-4'>
-        <div className='card-body'>
-          <h3>Upload an Image</h3>
-          <p>Upload an image to classify the bird species</p>
-          <form onSubmit={handleSubmit}>
-            <input type='file' className='form-control mb-3' 
-              onChange={handleChange}
-            />
-            <div className='my-3'>
-              <button className='btn btn-success rounded-0 w-100'>
-                Upload
-              </button>
+    <div className="container">
+      <div className="col-md-4 offset-md-4">
+        {loading && (
+          <div className="progress rounded-0">
+            <div
+              className="progress-bar progress-bar-striped bg-success"
+              role="progressbar"
+              style={{ width: `${uploadPercentage}%` }}
+            >
+              {uploadPercentage}%
             </div>
-          </form>
+          </div>
+        )}
+        <div className="card bg-dark text-light rounded-0 p-4">
+          <div className="card-body">
+            <h3>Upload an Image</h3>
+            <p>Upload an image to classify the bird species</p>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="file"
+                className="form-control mb-3"
+                accept=".png, .jpg, .jpeg"
+                onChange={handleChange}
+              />
+              <div className="my-3">
+                <button className="btn btn-success rounded-0 w-100">
+                  Upload
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
+      <div className="container">
+        {isData && <PredictionModel imageURL={image} prediction={prediction} />}
+        \\
+        
       </div>
     </div>
   );
